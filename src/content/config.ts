7ebad968c5c9a81bf94afd035c6cdbcd2417a714
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-03-13 13:09:23 +0100

    Initial commit

---
+++ src/content/config.ts
@@ -0,0 +1,33 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const postsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    published: z.date(),
+    description: z.string(),
+    tags: z.array(z.string()),
+    updated: z.date().optional(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }).optional(),
+  }),
+});
+
+const infoCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    published: z.date(),
+    updated: z.date().optional(),
+  }),
+});
+
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  "posts": postsCollection,
+  "info": infoCollection,
+};
