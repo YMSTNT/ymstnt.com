961a4cc22240f57b7f7715c73194be5de5115565
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-03-13 13:09:23 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://ymstnt.com',
+  redirects: {
+    '/érettségi': '/erettsegi',
+    '/egyetem': '/uni',
+    '/university': '/uni',
+  }
+});
